417792ca52b6b77fede0b6625a19ae6be07d7319
Andrew Monks <a@monks.co>
2013-03-31 19:41:37 -0500

    updates

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
 maxkit
 ======
 
-max patches for easier hardware connectivity
+## instructions
 
-## wiimote
+*	put maxkit somewhere in your max path (ie /Applications/Max6/Cycling '74/)
 
-![screenshot](http://f.cl.ly/items/0X2J3x2F0c032T46230b/Screen%20Shot%202012-11-28%20at%207.37.47%20PM.png)
+*	restart Max
 
-wiimote patch for max/msp
+## max patches for easier hardware connectivity
 
-## nanokontrol
+### arduino
+
+**cereal.maxpat** is an abstraction that takes serial input (with settable port, baud, and update frequency) parses the packet into a list. Serial messages can be an arbitrarily long list of numbers with entries split by spaces.
+
+**interface.maxpat** and **interface.ino** give an example of using cereal.maxpat and automap.maxpat together to map arbitrary-resistance sensors plugged into an arduino into 6 1-127 integer values in Max.
+
+### automap
+
+automap.maxpat is an abstraction that maps an arbitrary range of values to a set output range. When banged, it sets the input range based on the range of input received since the last bang. 
+
+### nanokontrol
 
 ![screenshot](http://cl.ly/LDy0/Screen%20Shot%202012-11-28%20at%207.40.56%20PM.png)
 
 nanokontrol v1 patch for max/msp
+
+### wiimote
+
+![screenshot](http://f.cl.ly/items/0X2J3x2F0c032T46230b/Screen%20Shot%202012-11-28%20at%207.37.47%20PM.png)
+
+wiimote patch for max/msp
---
+++ interface/interface.ino
@@ -0,0 +1,71 @@
+/*
+  Analog Input
+ Demonstrates analog input by reading an analog sensor on analog pin 0 and
+ turning on and off a light emitting diode(LED)  connected to digital pin 13. 
+ The amount of time the LED will be on and off depends on
+ the value obtained by analogRead(). 
+ 
+ The circuit:
+ * Potentiometer attached to analog input 0
+ * center pin of the potentiometer to the analog pin
+ * one side pin (either one) to ground
+ * the other side pin to +5V
+ * LED anode (long leg) attached to digital output 13
+ * LED cathode (short leg) attached to ground
+ 
+ * Note: because most Arduinos have a built-in LED attached 
+ to pin 13 on the board, the LED is optional.
+ 
+ 
+ Created by David Cuartielles
+ modified 30 Aug 2011
+ By Tom Igoe
+ 
+ This example code is in the public domain.
+ 
+ http://arduino.cc/en/Tutorial/AnalogInput
+ 
+ */
+
+int sensorPin0 = A0;    // select input pins
+int sensorPin1 = A1;    // 
+int sensorPin2 = A2;    // 
+int sensorPin3 = A3;    // 
+int sensorPin4 = A4;    // 
+int sensorPin5 = A5;    // 
+
+
+int sensorValue0 = 0;   // set pin values to 0
+int sensorValue1 = 0;   // 
+int sensorValue2 = 0;   // 
+int sensorValue3 = 0;   // 
+int sensorValue4 = 0;   // 
+int sensorValue5 = 0;   // 
+
+void setup() {
+  Serial.begin(9600);   // start serial connection at 9600   
+}
+
+void loop() {
+
+  sensorValue0 = analogRead(sensorPin0);  // read input values
+  sensorValue1 = analogRead(sensorPin1);    
+  sensorValue2 = analogRead(sensorPin2);    
+  sensorValue3 = analogRead(sensorPin3);    
+  sensorValue4 = analogRead(sensorPin4);    
+  sensorValue5 = analogRead(sensorPin5);    
+  
+  Serial.print(sensorValue0); // print list of input values to serial port
+  Serial.print(" ");
+  Serial.print(sensorValue1);
+  Serial.print(" ");
+  Serial.print(sensorValue2);
+  Serial.print(" ");
+  Serial.print(sensorValue3);
+  Serial.print(" ");
+  Serial.print(sensorValue4);
+  Serial.print(" ");
+  Serial.println(sensorValue5);
+  
+  delay(10);                  
+}
